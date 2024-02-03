2c9ccb8222c4f0af91d091733e22269679956b76
oussamagha13 <133254806+oussamagha13@users.noreply.github.com>
2024-02-03 19:13:16 +0100

    Add files via upload

---
+++ webpack.mix.js
@@ -0,0 +1,6 @@
+// webpack.mix.js
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
+   .copy('node_modules/bootstrap/dist/css/bootstrap.min.css', 'public/css');
